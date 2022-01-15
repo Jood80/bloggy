1bef42bc9134dbdb13f6b01a3f23557d4640cc08
Jood80 <shawar.nujood@gmail.com>
2022-01-15 13:43:15 +0200

    Feat(pages): create 404 & landing pages

---
+++ client/src/pages/404.js
@@ -0,0 +1,22 @@
+import { Button, Center, Box } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
+import { ReactComponent as Error } from '../images/error-404.svg'
+
+
+export default function NotFound() {
+  return (
+    <Center >
+      <Box textAlign={'center'}>
+        <Error style={{ width: "35rem", height: "32rem" }} />
+        <Button variant={'solid'}
+          colorScheme={'teal'}
+          size={'md'}
+        ><Link to='/'>
+            ¯\_(ツ)_/¯ <br />
+            Back Home
+          </Link>
+        </Button>
+      </Box>
+    </Center>
+  )
+}
