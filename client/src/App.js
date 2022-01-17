5f108c0a5e35963ca2683ed2941aa1ddd59a72c3
Jood80 <shawar.nujood@gmail.com>
2022-01-17 22:29:21 +0200

    Feat(loader): add loader to the layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
+import { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom'
-import { LandingPage, Signup, Signin, Home, NotFound, ResetPassword } from './pages'
+import { Signup, Signin, NotFound, ResetPassword } from './pages'
 import { AuthProvider } from './contexts/AuthContext'
 import PrivateRoute from './utils/private-route';
 import LandingLayout from './components/layout';
 
+const Home = lazy(() => import('./pages/home'))
+const LandingPage = lazy(() => import('./pages/landing-page'))
 
 
 function App() {
