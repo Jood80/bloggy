b94753269c8f004f1a624f402ab2bd9679c57e0b
Jood80 <shawar.nujood@gmail.com>
2022-01-18 21:38:45 +0200

    Chore(*): update the migrations

---
+++ server/migrations/20220118152440-create_users_table.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable("users", {
+      id: {
+        type: Sequelize.INTEGER(11),
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+      },
+      username: {
+        type: Sequelize.STRING(35),
+        allowNull: false,
+        unique: true
+      },
+      email: {
+        type: Sequelize.STRING(40),
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      phone: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      avatar: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+        allowNull: false
+      }
+      , updatedAt: Sequelize.DATE
+    })
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    return queryInterface.dropTable("users")
+  }
+};
