ed5c26796eac300fffe12deb763feb84fd2b65be
Jood80 <shawar.nujood@gmail.com>
2022-01-25 23:34:49 +0200

    Chore(query): update GET a specific user's tweets

--- server/src/controllers/tweets/getUserTweets.js
+++ server/src/controllers/tweets/getUserTweets.js
@@ -3,13 +3,16 @@ const { Tweet } = require('../../database/models');
 module.exports = async (req, res, next) => {
   try {
     const { userId } = req.params
-    const allTweets = await Tweet.findAll({
+    const userTweets = await Tweet.findAll({
+      attributes: ['content', 'createdAt'],
+      order: [
+        ['createdAt', 'DESC'],
+      ],
       where: {
         userId,
       },
-      include: [{ model: Tweet, as: "tweets" }]
     })
-    res.json({ allTweets })
+    res.status(200).json(userTweets)
   } catch (error) {
     next(error)
   }
