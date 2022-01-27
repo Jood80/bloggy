d78c2bdf4f41adf1f95b1a0e536d92152b89cc06
Jood80 <shawar.nujood@gmail.com>
2022-01-27 13:03:14 +0200

    Chore(cors): update cors settings &use compression

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const { join } = require('path')
 const express = require('express')
 const cors = require('cors')
+const compression = require('compression')
 const router = require('./src/routes')
 
 const app = express()
@@ -8,12 +9,20 @@ const app = express()
 app.use(express.static(join(__dirname, '..', 'client', 'build')))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use('/api', router);
 
+app.use(cors({
+  origin: process.env.WHITELIST,
+  methods: process.env.ALLOWED_METHODS,
+  credentials: true,
+  allowedHeaders: 'Content-Type, Authorization',
+  maxAge: 86400,
+}))
+
+app.use(compression());
+app.use('/api', router);
 
 app.get('*', (_req, res) => {
   res.sendFile(join(__dirname, '..', 'client', 'build', 'index.html'))
 })
-app.use(cors())
 
 module.exports = app
